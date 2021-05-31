2aabb15830485239216ee62fbca9ed4129a0dc29
Kamil146 <kamilgomulka@gmail.com>
2021-05-31 18:28:13 +0200

    Kod sprawozdanie

---
+++ funkcje.py
@@ -0,0 +1,105 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def DefinicjaGeometrii():
+    wezly = np.array([[1, 0],
+                      [2, 1],
+                      [3, 0.5],
+                      [4, 0.75]])
+    elementy = np.array([[1, 1, 3],
+                         [2, 4, 2],
+                         [3, 3, 4]])
+    n = np.shape(wezly)[0]
+
+    return wezly,elementy,n
+
+def AutomatycznaGeometria(p,k,n):
+    # p - poczatek przedzialu
+    # k - koniec przedzialu
+    # n - liczba wezlow
+    tmp = (k-p) / (n-1) # odległośc pomiedzy dwoma wezlami
+    matrix = np.array([1,p]) # zapisujemy pierwszy wezel - pierwszz kolumna indeksy, druga pozycja wezla
+    matrix2 = np.array([1,1,2]) # zapisujemy elementy - pierwsza indeks elementu, kolejna poczatek i koniec
+
+    for i in range(1, n, 1): # start 1, koniec n , krok 1 - dodajemy nowe wiersza, gdzie bedzie indeks (i+1) oraz wspolrzedna wezla
+        matrix = np.block([
+            [matrix],
+            [i+1, i * tmp + p],
+        ])
+    for i in range (2,n,1): #
+        matrix2 = np.block([
+            [matrix2],
+            [i,i,i+1]
+        ])
+
+    return matrix, matrix2
+
+def RysowanieGeometrii(wezly,elementy):
+    y = np.zeros(wezly.shape[0])
+    plt.plot(wezly[:, 1], y, marker='o')
+    for i in range(0, np.size(y), 1):
+             plt.text(x=wezly[i, 1], y=y[i] + 0.010, s=int(wezly[i, 0]), color='red')
+    for i in range(0, np.size(y), 1):
+             plt.text(x=wezly[i, 1], y=y[i] - 0.010, s=str(round(wezly[i, 1], 2)), color='green')
+    for i in range(0, np.size(y) - 1, 1):
+             wp = elementy[i, 1]
+             wk = elementy[i, 2]
+             plt.text(x=(wezly[wp-1, 1] + wezly[wk-1, 1]) / 2, y=y[i] + 0.005, s=int(i + 1), color='blue')
+
+    plt.show()
+
+
+def Aij(df_i, df_j, c, f_i, f_j):
+    '''
+    Parametry :
+    df_i , df_j - pochodne funkcji ksztaltu zwiazanych z i-tym oraz j- tym wezlem
+    Zwraca :
+    wyrazenie lambda bedace funkcja podcalkowa A_ij
+    '''
+
+    f_pod = lambda x: -df_i(x)*df_j(x) + c*f_i(x)*f_j(x)
+
+    return f_pod
+
+
+def Alokacja(n):
+
+    A = np.zeros([n, n])
+    b = np.zeros([n, 1])
+
+    return A, b
+
+
+def fBazowe(n):
+
+    if n == 0:
+        f = (lambda x: 0 * x+1)
+        df = (lambda x: 0 * x)
+
+    elif n == 1:
+
+        f = (lambda x: -0.5 * x + 1 / 2, lambda x: 0.5 * x + 0.5)
+        df = (lambda x: -1 / 2 + 0 * x, lambda x: 0.5 + 0 * x)
+
+    elif n == 2:
+        f = (lambda x: 0.5 * x ** 2 - 0.5 * x, lambda x: -x ** 2 + 1, lambda x: 0.5 * x ** 2 + 0.5 * x)
+        df = (lambda x: x - 0.5, lambda x: -2 * x, lambda x: x + 0.5)
+
+    else:
+        raise Exception("Nastapil blad w fBazowe().")
+
+    return f, df
+
+
+def Rozwiazanie(wezly, elementy, u):
+
+    x = wezly[:, 1]
+    plt.plot(x, u, 'm*')
+    RysowanieGeometrii(wezly, elementy)
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,130 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as spint
+import funkcje as fun
+
+if __name__ == '__main__':
+
+    # Preprocessing
+
+    ## parametry sterujace
+    c = 0
+    f = lambda x: 0 * x  # wymuszenie
+
+
+
+    #WEZLY, ELEMENTY,n =  fun.DefinicjaGeometrii()
+    # n = np.shape(WEZLY)[0]
+    n = 6
+    x_a = 0
+    x_b = 6
+    # warunki brzegowe
+    WB = [{"ind": 1, "typ": 'D', "wartosc": 1},
+          {"ind": n, "typ": 'D', "wartosc": 2}]
+    ## lub Automatyczne wygenerowanie geometrii
+
+    WEZLY, ELEMENTY = fun.AutomatycznaGeometria(x_a, x_b, n)
+
+
+
+
+    fun.RysowanieGeometrii(WEZLY, ELEMENTY)
+
+    A, b = fun.Alokacja(n)
+
+
+
+    stopien = 1
+    phi, dphi = fun.fBazowe(stopien)
+
+    # #wyswietlenie funkcji i pochodnych
+    z = np.linspace(-1,1, 101)
+    plt.plot(z, phi[0](z), 'pink' )
+    plt.plot(z, phi[1](z), 'gray' )
+    #plt.plot(z, phi[2](z), 'orange')
+    plt.show()
+    plt.plot(z, dphi[0](z), 'black' )
+    plt.plot(z, dphi[1](z), 'brown' )
+    #plt.plot(z, dphi[2](z), 'green')
+    plt.show()
+
+
+
+
+    liczba_elem = np.shape(ELEMENTY)[0]
+    for ee in np.arange(0, liczba_elem):
+        elemGlobalInd = ELEMENTY[ee, 0]
+        elem_wez_pocz = ELEMENTY[ee, 1]
+        elem_wez_konc = ELEMENTY[ee, 2]
+        indGlobalneWezlow = np.array([elem_wez_pocz, elem_wez_konc])
+        x_a = WEZLY[elem_wez_pocz - 1, 1]
+        x_b = WEZLY[elem_wez_konc - 1, 1]
+        M = np.zeros([stopien + 1, stopien + 1])
+        J = (x_b - x_a) / 2
+        m = 0
+        n = 0
+        M[m, n] = J * spint.quad(fun.Aij(dphi[m], dphi[n], c, phi[m], phi[n]), -1, 1)[0]
+        m = 0
+        n = 1
+        M[m, n] = J * spint.quad(fun.Aij(dphi[m], dphi[n], c, phi[m], phi[n]), -1, 1)[0]
+        m = 1
+        n = 0
+        M[m, n] = J * spint.quad(fun.Aij(dphi[m], dphi[n], c, phi[m], phi[n]), -1, 1)[0]
+        m = 1
+        n = 1
+        M[m, n] = J * spint.quad(fun.Aij(dphi[m], dphi[n], c, phi[m], phi[n]), -1, 1)[0]
+        A[np.ix_(indGlobalneWezlow - 1, indGlobalneWezlow - 1)] += M
+
+
+
+
+    #UWZGLEDNIENIE WARUNKOW BRZEGOWYCH
+    if WB[0]['typ'] == 'D'and WB[1]['typ'] == 'D' :
+        ind_wezla_pocz = WB[0]['ind']
+        ind_wezla_konc = WB[1]['ind']
+        wart_war_brzeg_pocz = WB[0]['wartosc']
+        wart_war_brzeg_konc = WB[1]['wartosc']
+
+        iwp_p = ind_wezla_pocz - 1
+        iwp_k = ind_wezla_konc -1
+        A=np.delete(A,[iwp_p,iwp_k],0)
+
+        b = np.delete(b,[iwp_p,iwp_k], 0)
+        wb=np.shape(b)[0]
+        for i in np.arange(0,wb):
+            b[i]=b[i]-A[i,iwp_p]*wart_war_brzeg_pocz
+        for j in np.arange(0,wb):
+            b[j]=b[j]-A[j,iwp_k]*wart_war_brzeg_konc
+        A = np.delete(A, [iwp_p, iwp_k], 1)
+
+
+    # if WB[0]['typ'] == 'D':
+    #     ind_wezla = WB[0]['ind']
+    #     wart_war_brzeg = WB[0]['wartosc']
+    #
+    #     iwp = ind_wezla - 1
+    #
+    #     WZMACNIACZ = 10 ** 14
+    #
+    #     b[iwp] = A[iwp, iwp] * WZMACNIACZ * wart_war_brzeg
+    #     A[iwp, iwp] = A[iwp, iwp] * WZMACNIACZ
+    #
+    # if WB[1]['typ'] == 'D':
+    #     ind_wezla = WB[1]['ind']
+    #     wart_war_brzeg = WB[1]['wartosc']
+    #
+    #     iwp = ind_wezla - 1
+    #
+    #     WZMACNIACZ = 10 ** 14
+    #
+    #     b[iwp] = A[iwp, iwp] * WZMACNIACZ * wart_war_brzeg
+    #     A[iwp, iwp] = A[iwp, iwp] * WZMACNIACZ
+
+
+
+    # Rozwiazanie ukl row lin
+
+    u = np.linalg.solve(A, b)
+    u=np.vstack(([wart_war_brzeg_pocz],u,[wart_war_brzeg_konc])) #przy drugim sposobie musimy uwzglednic
+    print(u)
+    fun.Rozwiazanie(WEZLY, ELEMENTY,u)
